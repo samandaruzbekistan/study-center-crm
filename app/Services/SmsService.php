2eb69ce15cf0f9e0d1897a93a6071fe10e0cc5bf
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2023-09-30 03:49:26 +0500

    add time based access middleware

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -275,4 +275,42 @@ class SmsService
         }
     }
 
+    public function apiSMS($number, $code){
+        $number = preg_replace('/\D/', '', $number);
+        $token = SmsConfig::find(1);
+        $current_date = Carbon::now();
+        $token_expiry_date = Carbon::parse($token->updated_at)->addMonth();
+        if($current_date->greaterThan($token_expiry_date)){
+            $re = $this->getToken();
+            if ($re['message'] == 'error') return response()->json(['message'=> 'error'], 200);
+        }
+        $token = $token->token;
+        $user = new Client();
+        $headers = [
+            'Authorization' => "Bearer {$token}"
+        ];
+        $options1 = [
+            'multipart' => [
+                [
+                    'name' => 'mobile_phone',
+                    'contents' => "{$number}"
+                ],
+                [
+                    'name' => 'message',
+                    'contents' => "MyTok dasturida tasdiqlash kodi: {$code}"
+                ],
+                [
+                    'name' => 'from',
+                    'contents' => '4546'
+                ],
+                [
+                    'name' => 'callback_url',
+                    'contents' => 'http://0000.uz/test.php'
+                ]
+            ]];
+        $request = new \GuzzleHttp\Psr7\Request('POST', 'notify.eskiz.uz/api/message/sms/send', $headers);
+        $res = $user->sendAsync($request,$options1)->wait()->getBody()->getContents();;
+        return $res;
+    }
+
 }
